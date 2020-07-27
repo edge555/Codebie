d84ceef74222dbaa4530975fd35184611fdfd009
Shoaib Ahmed <2010.ahmed.shoaib@gmail.com>
2020-07-27 12:43:59 +0600

    Add express handlebars and Some routes

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const app = express();
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+    defaultLayout:'main'
+}));
+app.set('view engine', 'handlebars');
+
+// Middleware
+app.use(function(req,res,next){
+    next();
+});
+
+// Index route
+app.get('/',function(req,res){
+    res.render('index');
+});
+
+// About route
+app.get('/about',function(req,res){
+   res.render('about');
+});
+
 app.listen(3000,function(){
     console.log("Server started on port 3000");
 });
